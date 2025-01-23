108405d6197e1cdab2fc3b49282302adfe091a34
Vinod <118246607+vinodvk00@users.noreply.github.com>
2025-01-23 18:27:39 +0530

    Update api.js

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export default axios.create ({
-    baseURL: "http://localhost:7001/api",
+    // baseURL: "http://localhost:7001/api",
+    baseURL: "https://multi-factor-auth.onrender.com/api",
 });
